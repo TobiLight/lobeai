4ac793b0e001144292e378d892ea2e307264f463
TobiLight <jgetitdonefast@gmail.com>
2023-11-26 02:11:35 +0100

    ...

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -129,6 +129,7 @@ async def decode_google_token(request: Request) -> UserProfile:
                                 detail="Token has expired!", headers={"Authorization": "Bearer"})
         return None
 
+    print(id_info)
     if id_info["iss"] == "https://accounts.google.com":
         try:
             existing_user = await db.user.find_first(where={"email": id_info["email"]})
