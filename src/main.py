206eb57d2e7cff1d2caaa9ff0b74a2cd72155654
TobiLight <jgetitdonefast@gmail.com>
2023-11-27 17:00:39 +0100

    ...

--- src/main.py
+++ src/main.py
@@ -11,21 +11,21 @@ from prisma import errors
 def init_app():
     """Initialize app"""
     app = FastAPI(version="1.0.0")
-    # origins = [
-    #     "*",
-    #     "https://stuttern-hackathon-fe-g179.vercel.app/",
-    #     "https://stuttern-hackathon-fe-g179.vercel.app",
-    #     "http://localhost.tiangolo.com",
-    #     "https://localhost.tiangolo.com",
-    #     "http://localhost",
-    #     "http://localhost:8080",
-    # ]
+    origins = [
+        # "*",
+        "https://stuttern-hackathon-fe-g179.vercel.app/",
+        "https://stuttern-hackathon-fe-g179.vercel.app",
+        "http://localhost.tiangolo.com",
+        "https://localhost.tiangolo.com",
+        "http://localhost",
+        "http://localhost:8080",
+    ]
 
-    # app.add_middleware(CORSMiddleware,
-    #                    allow_origins=origins,
-    #                    allow_credentials=True,
-    #                    allow_methods=["*"],
-    #                    allow_headers=["*"],)
+    app.add_middleware(CORSMiddleware,
+                       allow_origins=origins,
+                       allow_credentials=True,
+                       allow_methods=["*"],
+                       allow_headers=["*"],)
 
     @app.on_event("startup")
     async def startup():
