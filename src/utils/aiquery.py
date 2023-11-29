7993fa9065a93492499d51f6cb18970f2248acb4
TobiLight <jgetitdonefast@gmail.com>
2023-11-29 01:51:20 +0100

    code cleanup

--- src/utils/aiquery.py
+++ src/utils/aiquery.py
@@ -38,7 +38,7 @@ def generate_mongo(query_text: str, table_meta: Dict[str, str]):
         messages=[
             {
                 "role": "system",
-                "content": "You are a helpful assistant that knows a lot about NoSQL language and manages a MongoDB database. \nYou are using PyMongo (a mongodb python package) as the ODM.\n\n You should respond with the correct command query using 'command' function. You MUST answer only with a correct PyMongo command query (using the command function pymongo has) and don't wrap it into a code block. Don't include any explanation.\n Today is {0}.\n\n The database tables are: {1}. The table is a hashmap of table name as keys and the schemas as values.".format(datetime.now().date(), table_meta)
+                "content": "You are a helpful assistant that knows a lot about NoSQL language and manages a MongoDB database. \nYou are using PyMongo (a mongodb python package) as the ODM.\n\n You MUST answer only with a correct PyMongo command query (using the command() function pymongo has) and don't wrap it into a code block. Don't include any explanation.\n Today is {0}.\n\n The database tables are: {1}. The table is a hashmap of table name as keys and the schemas as values.".format(datetime.now().date(), table_meta)
             },
             {
                 "role": "user",
@@ -79,7 +79,7 @@ def query_response_to_nl(query_text: str, query_answer: any):
             messages=[
                 {
                     "role": "system",
-                    "content": "You will be given a query and the result of executing the query on a database. Your response should be in HTML syntax explaining the result, note that the results would be set as inner HTML; so it should not include parent tags like DOCTYPE, body, etc. Maximum font size should be 1rem. Also, do not use dark colors.\n\nTry to explain in a detailed manner such that a non tech savvy user would understand, you can try to add tables and charts well formatted in HTML that a user can understand. You can also include any piece of HTML UI that would aid the explanation."
+                    "content": "You will be given a query and the result of executing the query on a database. Your response should be in HTML syntax explaining the result, note that the results would be set as inner HTML; so it should not include parent tags like DOCTYPE, body, etc. Maximum font size should be 1rem. Also, use white or bright colors.\n\nTry to explain in a detailed manner such that a non tech savvy user would understand, you can try to add tables and charts well formatted in HTML that a user can understand. You can also include any piece of HTML UI that would aid the explanation."
                 },
                 {
                     "role": "user",
